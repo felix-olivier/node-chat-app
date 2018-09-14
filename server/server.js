e839141b7e7cedc5ebf6ff1e549045eae289bec3
Felix van Deelen <felixvandeelen@hotmail.com>
2018-09-14 13:56:25 +0200

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
